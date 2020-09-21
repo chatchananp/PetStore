3ccbbbadbae7f7f498344c798acf25a2650b5857
LAPTOP-3KA930G9\baschatt <baschattkrab@gmail.com>
2020-09-21 10:30:27 +0700

    add handle error for add pet to database

--- src/main/java/com/petstore/exception/GlobalExceptionHandler.java
+++ src/main/java/com/petstore/exception/GlobalExceptionHandler.java
@@ -1,7 +1,9 @@
 package com.petstore.exception;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -22,10 +24,17 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
     }
  
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, 
+    		HttpHeaders headers, HttpStatus status, WebRequest request) {
+    	ErrorDetails errorDetails = new ErrorDetails(HttpStatus.BAD_REQUEST.value(), "Validation Failed",
+    			ex.getBindingResult().toString());
+    	return new ResponseEntity<Object>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
+    
     @ExceptionHandler(MethodArgumentNotValidEx.class)
-    public ResponseEntity<?> handleMethodArgumentNotValid(MethodArgumentNotValidEx ex, WebRequest request) {
+    public ResponseEntity<?> anotherHandleMethodArgumentNotValid(MethodArgumentNotValidEx ex, WebRequest request) {
            ErrorDetails errorDetails = new ErrorDetails(HttpStatus.BAD_REQUEST.value(), ex.getMessage(), request.getDescription(false));
            return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
     }
-    
 }
